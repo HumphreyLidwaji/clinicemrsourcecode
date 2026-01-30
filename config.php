46a190d4c0742848bd9d79493a21af78bd0d7976
Ro0t <111574595+HumphreyLidwaji@users.noreply.github.com>
2026-01-30 08:25:28 +0300

    Update database credentials and installation ID

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
 $dbhost = 'localhost';
-$dbusername = 'itflow';
-$dbpassword = 'P@$$c0de!';
+$dbusername = 'clinicemr';
+$dbpassword = 'strong@1234';
 $database = 'itflow';
 $mysqli = mysqli_connect($dbhost, $dbusername, $dbpassword, $database) or die('Database Connection Failed');
 $config_app_name = 'ClinicEMR';
 $config_base_url = 'localhost/setup';
 $config_https_only = FALSE;
 $repo_branch = 'master';
-$installation_id = '8hLoAkbIyY1b6LWvY5AaJmqF6BrRZKRJ';
+$installation_id = '';
 $config_enable_setup = 0;
 
